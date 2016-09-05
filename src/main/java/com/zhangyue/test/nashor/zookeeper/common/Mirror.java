cc1bfe7a6e0f89d9e2e7cca4c41e1555edf2c4f8
yanmeng <yanmeng@zhangyue.com>
2016-09-05 19:04:45 +0800

    提交

---
+++ src/main/java/com/zhangyue/test/nashor/zookeeper/common/Mirror.java
@@ -0,0 +1,11 @@
+package com.zhangyue.test.nashor.zookeeper.common;
+
+/**
+ * 镜像, 被容器所装载, 每个容器只能装载一个镜像
+ *
+ * @author YanMeng
+ * @date 16-9-5
+ */
+public class Mirror {
+
+}
---
+++ src/main/java/com/zhangyue/test/nashor/zookeeper/example/LeaderElect.java
@@ -0,0 +1,9 @@
+package com.zhangyue.test.nashor.zookeeper.example;
+
+/**
+ * ZK实现Leader选举
+ * @author YanMeng
+ * @date 16-9-5
+ */
+public class LeaderElect {
+}
